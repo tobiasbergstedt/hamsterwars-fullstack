e9dad174fa2d17864611306fecb8fbba5880567c
Tobias Bergstedt <info@tobiasbergstedt.se>
2022-05-27 07:57:06 +0200

    Added readme.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hamster Wars
+
+Fullstack app where you can vote for the cutest hamster, add new hamsters, and see statistics for all hamsters and battles.
+
+### Frontend
+
+React and Recoil.
+
+### Backend
+
+Node and Express.
+
+### Database
+
+Used Firestore to host the database.
--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,11 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Router>
       <RecoilRoot>
       <App />
       </RecoilRoot>
     </Router>
-  // </React.StrictMode>
+  </React.StrictMode>
 )
